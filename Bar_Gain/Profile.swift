2c558a68faa4ae18a7d9373b772be6bf1e2982bb
Asaf Fisher <asaffisher.dev@gmail.com>
2017-09-12 13:04:58 -0400

    Started to work on the Page Change of the profile.

--- Bar_Gain/Profile.swift
+++ Bar_Gain/Profile.swift
@@ -8,26 +8,73 @@
 
 import UIKit
 
-class Profile: UIViewController {
+class Profile: UIViewController,UIScrollViewDelegate {
 
     @IBOutlet weak var userProfileImage: UIImageView!
     
+    let imagelist = ["image1.jpg", "image1.jpg"]
+    var scrollView = UIScrollView()
+    
+    var pageControl : UIPageControl = UIPageControl(frame:CGRect(x: 50, y: 300, width: 200, height: 50))
+    
+    var yPosition:CGFloat = 0
+    var scrollViewContentSize:CGFloat=0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         userProfileImage.layer.cornerRadius = 25
         userProfileImage.clipsToBounds = true
         
-
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 300))
+        
+        configurePageControl()
+        
+        scrollView.delegate = self
+        self.view.addSubview(scrollView)
+        for  i in stride(from: 0, to: imagelist.count, by: 1) {
+            var frame = CGRect.zero
+            frame.origin.x = self.scrollView.frame.size.width * CGFloat(i)
+            frame.origin.y = 0
+            frame.size = self.scrollView.frame.size
+            self.scrollView.isPagingEnabled = true
+            
+            let myImage:UIImage = UIImage(named: imagelist[i])!
+            let myImageView:UIImageView = UIImageView()
+            myImageView.image = myImage
+            myImageView.contentMode = UIViewContentMode.scaleAspectFit
+            myImageView.frame = frame
+            
+            scrollView.addSubview(myImageView)
+        }
+        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.size.width * CGFloat(imagelist.count), height: self.scrollView.frame.size.height)
+        pageControl.addTarget(self, action: Selector(("changePage:")), for: UIControlEvents.valueChanged)
         // Do any additional setup after loading the view.
     }
-
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        self.pageControl.numberOfPages = imagelist.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.red
+        self.pageControl.pageIndicatorTintColor = UIColor.black
+        self.pageControl.currentPageIndicatorTintColor = UIColor.green
+        self.view.addSubview(pageControl)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-   
+    func changePage(sender: AnyObject) -> () {
+        let x = CGFloat(pageControl.currentPage) * scrollView.frame.size.width
+        scrollView.setContentOffset(CGPoint(x: x,y :0), animated: true)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = Int(pageNumber)
+    }
     /*
     // MARK: - Navigation
 
--- Bar_Gain/ResultTableView.swift
+++ Bar_Gain/ResultTableView.swift
@@ -20,5 +20,6 @@ class ResultTableView: UITableView {
     
     
     
+    
 
 }
