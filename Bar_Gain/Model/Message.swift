3bfdd8c40f1de64dadac314834adb6185223479f
JohnnyBis <gianmariabiselli@gmail.com>
2017-09-12 12:14:25 -0400

    Added Textfield delegate + animations in ChatMessageView.swift

---
+++ Bar_Gain/Model/Message.swift
@@ -0,0 +1,18 @@
+//
+//  Message.swift
+//  Bar_Gain
+//
+//  Created by Gianmaria Biselli on 9/12/17.
+//  Copyright © 2017 Gianmaria Biselli. All rights reserved.
+//
+
+import Foundation
+
+class Message{
+
+    
+    var sender : String = ""
+    var messageBody : String = ""
+    
+    
+}
