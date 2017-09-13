c4fa2c2a134a255ebdd6e5a6e156b645ebd7c99a
Asaf Fisher <asaffisher.dev@gmail.com>
2017-09-13 08:38:52 -0400

    Added Constants file

---
+++ Bar_Gain/Model/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Bar_Gain
+//
+//  Created by Asaf Fisher on 9/13/17.
+//  Copyright © 2017 Gianmaria Biselli. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct cells {
+        static let resultCell = ""
+    }
+}
