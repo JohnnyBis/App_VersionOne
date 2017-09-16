8785dcd609aa90007b49add5690a678156be7b83
JohnnyBis <gianmariabiselli@gmail.com>
2017-09-16 11:39:58 -0400

    Created Settings View Controller + log out button connected to Firebase

---
+++ Bar_Gain/Control/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  Bar_Gain
+//
+//  Created by Gianmaria Biselli on 9/16/17.
+//  Copyright © 2017 Gianmaria Biselli. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logOutButtonPressed(_ sender: UIButton) {
+        
+        do {
+            try FIRAuth.auth()?.signOut()
+            performSegue(withIdentifier: "goToSignIn", sender: self)
+        }
+        catch {
+            print("error: there was a problem signing out")
+        }
+        
+ 
+        
+    }
+    
+
+}
